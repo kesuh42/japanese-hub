8c45a24a4419ae4d093470f35df2fa20b4ada864
kesuh42 <kevinsuh2018@u.northwestern.edu>
2022-05-23 18:08:14 -0500

    react package.json fix, server now routes to react

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+app.use(express.static("client/build"));
+
 //DATABASE CODE HERE!!
 
 //Import the API routes from the routes folder
@@ -15,7 +17,7 @@ app.use(routes)
 
 //Send every other request to React 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(5000, () => {
