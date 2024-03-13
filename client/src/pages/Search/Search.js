9424ca9cf4f9fb5c452e8477e84651adb331f7fc
kesuh42 <kevinsuh2018@u.northwestern.edu>
2024-03-13 12:02:43 -0500

    form feedback

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,41 +1,54 @@
-import React from "react";
-import { TextField, Container } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { TextField, Container, Alert, Button } from '@mui/material';
 
-function Search () {
+function Search() {
+    const [FormFeedback, setFormFeedback] = useState(false)
     function handleSubmit(e) {
         e.preventDefault();
+        setFormFeedback(true)
         fetch(`${process.env.REACT_APP_BACKEND_URL}/JishoSearch?searchterm=` + `${e.target.elements.searchterm.value}`)
-        .then(response => response.json())
-        .then(data => {
-            fetch(`${process.env.REACT_APP_BACKEND_URL}/card`, 
-                {
-                    headers: {
-                      'Accept': 'application/json',
-                      'Content-Type': 'application/json'
-                    },
-                    method: "POST",
-                    body: JSON.stringify(data)
-                })
-            .then(response => console.log(response))
-        });
+            .then(response => response.json())
+            .then(data => {
+                fetch(`${process.env.REACT_APP_BACKEND_URL}/card`,
+                    {
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        method: "POST",
+                        body: JSON.stringify(data)
+                    })
+                    .then(response => console.log(response));
+            });
+    }
+
+    function AlertBox() {
+        return (
+        <Alert severity="success"
+            style={{
+                position: "absolute",
+                bottom: "0%",
+                left: "0%",
+            }}
+            onClose={() => { setFormFeedback(false) }}>
+            Card Created
+        </Alert>
+        );
     }
 
     return (
         <Container
-            style={{
-                position: "absolute",
-                top: "40%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-            }}>
+        >
             <form onSubmit={handleSubmit}>
                 <TextField id="outlined-basic" name="searchterm" label="Enter search term" variant="outlined" onSubmit={handleSubmit}
                     style={{
                         position: "absolute",
+                        top: "40%",
                         left: "50%",
                         transform: "translate(-50%, -50%)",
                     }} />
             </form>
+            {FormFeedback ? <AlertBox/> : null}
         </Container>
     );
 };
