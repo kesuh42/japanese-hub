87a46208c33e628a76d72be0b6dcd79dc929121f
kesuh42 <kevinsuh2018@u.northwestern.edu>
2024-03-15 08:56:26 -0500

    ready for deployment

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server is up on port 5000!");
 });
